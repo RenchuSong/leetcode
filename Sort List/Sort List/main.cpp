1f2ac0bd6a1da94a73955a4ad21c7ac22d5dcd15
RenchuSong <songrenchu@sina.com>
2014-07-19 18:22:03 -0700

    Sort List AC

--- Sort List/Sort List/main.cpp
+++ Sort List/Sort List/main.cpp
@@ -16,12 +16,8 @@ struct ListNode {
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
-int coun;
-
 void print(ListNode* node) {
 	if (node == NULL) return;
-	--coun;
-	if (coun < 0) return;
 	cout << node->val << " ";
 	print(node->next);
 }
@@ -29,35 +25,27 @@ void print(ListNode* node) {
 class Solution {
 public:
 	// Combine and result the last ListNode*
-    ListNode* combine(ListNode* pre, ListNode* x, int n, ListNode* y, int m) {
+    ListNode* combine(ListNode* pre, ListNode* left, ListNode* right, int len) {
 		int p = 0, q = 0;
-		for (int i = 0; i < n + m; ++i) {
-			if (q == m) {
-				pre->next = x;
-				for (int j = i; j < n + m; ++j) {
-					pre = pre->next;
-				}
-				break;
-			} else if (p == n) {
-				pre->next = y;
-				for (int j = i; j < n + m; ++j) {
-					pre = pre->next;
-				}
-				break;
-			} else{
-				cout << p << " " << q << endl;
-				if (x->val < y->val) {
-				++p;
-				pre->next = x;
-				x = x->next;
-				pre = pre->next;
-			} else {
+		for (int i = 0; i < len * 2; ++i) {
+			if (p == len) {
+				pre->next = right;
+				if (right != NULL) right = right->next;
 				++q;
-				pre->next = y;
-				y = y->next;
-				pre = pre->next;
+			} else if (q == len) {
+				pre->next = left;
+				left = left->next;
+				++p;
+			} else if (left->val < right->val) {
+				pre->next = left;
+				left = left->next;
+				++p;
+			} else {
+				pre->next = right;
+				right = right->next;
+				++q;
 			}
-			}
+			pre = pre->next;
 		}
 		return pre;
 	}
@@ -65,45 +53,55 @@ public:
 	ListNode *sortList(ListNode *head) {
         int n = 0;
 		ListNode* tmp = head;
+		int big = -1;
+		if (head != NULL) big = head->val;
 		while (tmp != NULL) {
+			if (tmp->val > big) big = tmp->val;
 			++n; tmp = tmp->next;
 		}
 		
 		if (n < 2) return head;
 		
-		ListNode* vh = new ListNode(-1);
-		vh->next = head;
-		
-		int len = 1;
-		while (len < n) {
-			int p = n / (len * 2);
-			int mod = n % (len * 2);
-
-			ListNode* x = vh->next, *y = vh->next, *nextx = NULL;
-			for (int i = 0; i < len; ++i) y = y->next;
-			if (p > 1) {
-				nextx = y;
-				for (int i = 0; i < len; ++i) nextx = nextx->next;
-			}
-			ListNode* pre = vh;
-			
-			for (int i = 0; i < p; ++i) {
-				pre = combine(pre, x, len, y, len);
-				x = nextx;
-				pre->next = nextx;
-				y = x;
-				if (i < p - 1 || mod > len)
-					for (int i = 0; i < len; ++i) y = y->next;
-				if (i < p - 1 || mod > len) {
-					nextx = y;
-					for (int i = 0; i < len; ++i) nextx = nextx->next;
-				}
-			}
-			if (mod > len) {
-				pre = combine(pre, x, len, y, mod - len);
-			}
-			len *= 2;
+		++big;
+		int nn = 1, exp = 0;
+		while (nn < n) {
+			nn *= 2;
+			++exp;
 		}
+		tmp = head;
+		for (int i = n; i < nn; ++i) {
+			ListNode* mid = new ListNode(big);
+			mid->next = tmp;
+			tmp = mid;
+		}
+		
+		ListNode* vh = new ListNode(-1);
+		vh->next = tmp;
+		
+		for (int loop = 0; loop < exp; ++loop) {
+			int len = 1 << loop;
+			int time = nn / len / 2;
+			//cout << time << endl;
+			//if (loop == 2)
+			//	print(vh);cout << endl;
+			ListNode *pre = vh, *nextStart = vh->next, *left = NULL, *right = NULL;
+			for (int i = 0; i < time; ++i) {
+				left = right = nextStart;
+				for (int j = 0; j < len; ++j) right = right->next;
+				if (i < time - 1) {
+					nextStart = right;
+					for (int j = 0; j < len; ++j) nextStart = nextStart->next;
+				}
+				//if (i == 0) cout << left->val << " " << right->val << endl;
+				pre = combine(pre, left, right, len);
+				//cout << pre->val << "---" << endl;
+			}
+			pre->next = NULL;
+		}
+		
+		tmp = vh;
+		for (int i = 0; i < n; ++i) tmp = tmp->next;
+		tmp->next = NULL;
 		
 		return vh->next;
     }
@@ -112,18 +110,18 @@ public:
 int main(int argc, const char * argv[])
 {
 	ListNode* p1 = new ListNode(10);
-	ListNode* p2 = new ListNode(2);
-	ListNode* p3 = new ListNode(3);
-	ListNode* p4 = new ListNode(4);
+	ListNode* p2 = new ListNode(6);
+	ListNode* p3 = new ListNode(5);
+	ListNode* p4 = new ListNode(40);
 	ListNode* p5 = new ListNode(5);
 	p1->next = p2;
 	p2->next = p3;
 	p3->next = p4;
 	p4->next = p5;
-	
+	//print(p1);
 	//(new Solution())->sortList(p1);
 	print((new Solution())->sortList(p1));
-	
+	cout << endl;
 	// insert code here...
 	std::cout << "Hello, World!\n";
     return 0;
